86cdedc21c23a7ed26aadc241b7e6d8c4ea1cd11
tarchan <tarchan@gmail.com>
2009-07-02 13:42:21 +0000

    lang/java/IRCKit: ライセンス文を追加
    git-svn-id: http://svn.coderepos.org/share/lang/java/IRCKit/trunk@34247 d0d07461-0603-4401-acd4-de1884942a52

--- src/com/mac/tarchan/net/irc/client/IRCClient.java
+++ src/com/mac/tarchan/net/irc/client/IRCClient.java
@@ -1,5 +1,29 @@
 /*
- * Copyright (c) 2009 tarchan. All rights reserved.
+ *  Copyright (c) 2009 tarchan. All rights reserved.
+ *  
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *  
+ *  1. Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *  
+ *  THIS SOFTWARE IS PROVIDED BY TARCHAN ``AS IS'' AND ANY EXPRESS OR
+ *  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ *  EVENT SHALL TARCHAN OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ *  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ *  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *  
+ *  The views and conclusions contained in the software and documentation are
+ *  those of the authors and should not be interpreted as representing official
+ *  policies, either expressed or implied, of tarchan.
  */
 package com.mac.tarchan.net.irc.client;
 
@@ -118,11 +142,12 @@ public class IRCClient
 	 * 環境設定を読み込みます。
 	 * 
 	 * @param name 設定ファイル名
+	 * @return このオブジェクト
 	 * @throws IOException 入力エラーが発生した場合
 	 * @see Properties#load(InputStream)
 	 * @see Properties#loadFromXML(InputStream)
 	 */
-	public void load(String name) throws IOException
+	public IRCClient load(String name) throws IOException
 	{
 		props = new Properties(DEFAULTS);
 		File file = new File(name);
@@ -150,6 +175,8 @@ public class IRCClient
 		{
 			in.close();
 		}
+
+		return this;
 	}
 
 	/**
@@ -271,10 +298,10 @@ public class IRCClient
 		String host = getProperty("irc.host");
 		String port = getProperty("irc.port");
 		URL url = new URL(String.format("irc://%s:%s", host, port));
-		System.out.format("[OPEN] %s\n", url);
+//		System.out.format("[OPEN] %s\n", url);
 		URLConnection con = url.openConnection();
 		con.connect();
-		System.out.format("[OPEN] %s\n", con);
+//		System.out.format("[OPEN] %s\n", con);
 
 		String encoding = getProperty("irc.encoding");
 		in = con.getInputStream();
@@ -433,6 +460,7 @@ public class IRCClient
 					break;
 				}
 			}
+			System.err.println("Disconnected.");
 		}
 
 		public void error(Exception x)
