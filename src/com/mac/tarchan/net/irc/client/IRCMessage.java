b8e7817a4f929ee68a7eec8f2a7184d877648bf1
tarchan <tarchan@d0d07461-0603-4401-acd4-de1884942a52>
2009-07-02 13:42:21 +0000

    lang/java/IRCKit: ライセンス文を追加
    git-svn-id: http://svn.coderepos.org/share/lang/java/IRCKit/trunk@34247 d0d07461-0603-4401-acd4-de1884942a52

--- src/com/mac/tarchan/net/irc/client/IRCMessage.java
+++ src/com/mac/tarchan/net/irc/client/IRCMessage.java
@@ -1,5 +1,29 @@
 /*
- * Copyright (c) 2009 tarchan. All rights reserved.
+ *  Copyright (c) 2009 tarchan. All rights reserved.
+ *  
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *  
+ *  1. Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *  
+ *  THIS SOFTWARE IS PROVIDED BY TARCHAN ``AS IS'' AND ANY EXPRESS OR
+ *  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ *  EVENT SHALL TARCHAN OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ *  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ *  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *  
+ *  The views and conclusions contained in the software and documentation are
+ *  those of the authors and should not be interpreted as representing official
+ *  policies, either expressed or implied, of tarchan.
  */
 package com.mac.tarchan.net.irc.client;
 
@@ -61,7 +85,8 @@ public class IRCMessage extends EventObject
 
 	// メッセージ解析結果
 	/** プレフィックス */
-	private String _prefix;
+//	private String _prefix;
+	private IRCName _prefix;
 
 	/** コマンド */
 	private String _command;
@@ -211,7 +236,7 @@ public class IRCMessage extends EventObject
 	 */
 	private void parsePrefix(String prefix)
 	{
-		_prefix = prefix;
+		_prefix = new IRCName(prefix);
 		String[] token = prefix.split(ADDRESS_DELIMITER);
 		if (token.length == 1)
 		{
@@ -384,7 +409,7 @@ public class IRCMessage extends EventObject
 	 * 
 	 * @return プレフィックス
 	 */
-	public String getPrefix()
+	public IRCName getPrefix()
 	{
 		return _prefix;
 	}
