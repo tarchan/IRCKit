f9b847c5a3a483f898dfce3180e639059af7bcc7
tarchan <tarchan@gmail.com>
2014-01-11 20:41:33 +0900

    URLStreamHandlerFactoryを名前解決するように修正

--- src/com/mac/tarchan/irc/bot/ShellAdapter.java
+++ src/com/mac/tarchan/irc/bot/ShellAdapter.java
@@ -6,7 +6,6 @@
 
 package com.mac.tarchan.irc.bot;
 
-import com.mac.tarchan.irc.IrcURLStreamHandlerFactory;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -24,10 +23,12 @@ public class ShellAdapter {
 
     public static void main(String[] args) {
         try {
-            URL.setURLStreamHandlerFactory(new IrcURLStreamHandlerFactory());
-//            URL url = new URL("irc://irc.ircnet.ne.jp/#javabreak");
+//            URL.setURLStreamHandlerFactory(new IrcURLStreamHandlerFactory());
+            System.setProperty("java.protocol.handler.pkgs", "com.mac.tarchan");
+            URL url = new URL("irc://irc.ircnet.ne.jp/javabreak");
 //            URL url = new URL("irc://tarchan:pass@irc.ircnet.ne.jp/#javabreak");
-            URL url = new URL("irc://irc.freenode.net/#javabreak");
+//            URL url = new URL("irc://irc.freenode.net/#javabreak");
+//            URL url = new URL("irc://irc.mozilla.org/firefox");
             URLConnection con = url.openConnection();
             con.addRequestProperty("channel", "#javabreak");
             con.addRequestProperty("channel", "#test");
